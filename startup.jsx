d25a5025c08f708193d177aa1cf8b4e81ded2f0e
Jaroslav Ondo <jaroslav.ondo@tik.local>
2018-10-16 10:04:39 +0200

    Initial commit

---
+++ startup.jsx
@@ -0,0 +1,12 @@
+﻿// © 2018 Ing. Jaroslav ONDO
+/**
+* @@@BUILDINFO@@@ startup.jsx !Version! Tue Oct 16 2018 07:46:26 GMT+0200
+*/
+
+#script "Startup"
+#includepath "../Prepress"
+#include "PrepressLib.jsxinc"
+#targetengine "session"
+
+// MENU
+prePressMenu ();
